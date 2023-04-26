359dd0c9d6a16ec816944842aec8b5c407ba2620
Udit Tyagi <57524054+UditTyagi455@users.noreply.github.com>
2023-04-26 17:29:11 +0000

    firebase auth added

--- src/auth/firebase-auth/index.js
+++ src/auth/firebase-auth/index.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC1Zb1JFCMEJM22bVFlUSep2KcqiPp9NXQ",
   authDomain: "using-firebase-8c9fc.firebaseapp.com",
+  databaseURL: "https://using-firebase-8c9fc-default-rtdb.firebaseio.com",
   projectId: "using-firebase-8c9fc",
   storageBucket: "using-firebase-8c9fc.appspot.com",
   messagingSenderId: "1092398978420",
@@ -13,5 +14,5 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 export default app ;
